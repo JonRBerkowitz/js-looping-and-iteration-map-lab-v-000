e706e501802048c93fcff55d61cb4ddf3749e6b1
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-11 18:48:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers (drivers) {
-  return drivers.map(toLowerCase();)
-};
+  return drivers.map(function(driver) {
+    return driver.toLowerCase();
+  });
+}
